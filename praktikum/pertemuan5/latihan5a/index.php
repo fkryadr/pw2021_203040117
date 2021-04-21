001f34b4e8b9211ad85cda9e1e5278ab3bd6173d
fkryadr <fkryadr@yandex.com>
2021-04-21 14:39:29 +0700

    add latihan5

---
+++ praktikum/pertemuan5/latihan5a/index.php
@@ -0,0 +1,18 @@
+<?php
+    include 'php/admin.php';
+    $produk = query("SELECT * FROM produk")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 5A</title>
+</head>
+<body>
+	<?php foreach ($produk as $prod) : ?>
+        </p>
+    <?php endforeach; ?>
+</body>
+</html>
